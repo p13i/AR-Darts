a461a49433ee6af9405f32bfef7cb93e0376471e
Pramod Kotipalli <pramod.kotipalli@gmail.com>
2018-06-18 23:20:52 -0400

    Remove unneeded class

---
+++ ARDarts/Materials.swift
@@ -0,0 +1,17 @@
+//
+//  Materials.swift
+//  ARDarts
+//
+//  Created by Pramod Kotipalli on 6/18/18.
+//  Copyright © 2018 Pramod Kotipalli. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Materials {
+    // The different materials used in the scene
+    static let detectedPlaneMaterial = UIColor(red: 1, green: 0, blue: 0, alpha: 0.5)
+    static let selectedPlaneMaterial = UIColor(red: 0, green: 0, blue: 1, alpha: 0.5)
+    static let dartboardMaterial = UIImage(named: "dartboard.png")
+}
--- ARDarts/ViewController.swift
+++ ARDarts/ViewController.swift
@@ -10,11 +10,10 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController {
 
+    var state: DartsGameState = .searchingForWalls
     @IBOutlet var sceneView: ARSCNView!
-    
-    var gameStateManager: DartsGameStateManager?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,10 +23,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
+        // Setttings for lighting...
         sceneView.automaticallyUpdatesLighting = true
         sceneView.autoenablesDefaultLighting = true
         
+        // Set to true to animate the scene
         self.sceneView.isPlaying = true
         
         // Create a new scene
@@ -36,11 +36,15 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the scene to the view
         sceneView.scene = scene
         
-        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        sceneView.debugOptions = [
+            ARSCNDebugOptions.showFeaturePoints,
+            ARSCNDebugOptions.showWorldOrigin,
+        ]
         
-        gameStateManager = DartsGameStateManager(for: sceneView)
+        let tapGestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(ViewController.throwDart(withTapGesture: )))
         
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.throwDart(withTapGesture: )))
         sceneView.addGestureRecognizer(tapGestureRecognizer)
     }
     
@@ -62,44 +66,138 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-    
     // Don't allow screen to rotate (make AR scene look bad for a few moments as it re-renders)
     override var shouldAutorotate: Bool { get { return false } }
 
-    // MARK: - ARSCNViewDelegate
+    // Maintains the currently selected plane (where the dartboard is placed)
+    var selectedPlane: ARPlaneAnchor?
+}
+
+// MARK: - ARSCNViewDelegate
+
+extension ViewController: ARSCNViewDelegate {
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
-        
     }
     
     func sessionWasInterrupted(_ session: ARSession) {
         // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
     }
     
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+    }
+
+    enum DartsGameState {
+        case searchingForWalls
+        case playingDarts
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        return self.gameStateManager!.handle(added: node, for: anchor)
+        // Only add new nodes to the scene if we are searching for walls
+        guard self.state == .searchingForWalls else { return }
+        
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        
+        // Add a new detected plane to the scene
+        let newPlaneNode = SCNNode()
+        newPlaneNode.geometry = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+        newPlaneNode.geometry?.firstMaterial?.diffuse.contents = Materials.detectedPlaneMaterial
+        newPlaneNode.position = SCNVector3(planeAnchor.center.x, 0, planeAnchor.center.z)
+        newPlaneNode.eulerAngles = SCNVector3(-1 * Float.pi / 2, 0, 0)
+        
+        node.addChildNode(newPlaneNode)
     }
     
     func renderer(_ renderer: SCNSceneRenderer, willUpdate node: SCNNode, for anchor: ARAnchor) {
-        return self.gameStateManager!.handle(updated: node, for: anchor)
+        // Only updated existing nodes if we are still searching for walls
+        guard self.state == .searchingForWalls else { return }
+        
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        
+        // Update the node's geometry and material
+        guard let updatedNode = node.childNodes.first else { return }
+        // Keep the same material
+        let updatedGeometry = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+        updatedGeometry.firstMaterial?.diffuse.contents = updatedNode.geometry?.firstMaterial?.diffuse.contents
+        updatedNode.geometry = updatedGeometry
+        updatedNode.position = SCNVector3(planeAnchor.center.x, 0, planeAnchor.center.z)
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
-        return self.gameStateManager!.handle(removed: node, for: anchor)
+        // Only updated existing nodes if we are still searching for walls
+        guard self.state == .searchingForWalls else { return }
     }
     
     @objc func throwDart(withTapGesture recognizer: UITapGestureRecognizer) {
-        return self.gameStateManager!.handle(tapWithRecognizer: recognizer)
+        let tapLocation = recognizer.location(in: self.sceneView)
+        switch self.state {
+        case .searchingForWalls:
+            self.hitTestWall(tapLocation)
+        case .playingDarts:
+            self.throwDart(tapLocation)
+        }
+    }
+    
+    private func hitTestWall(_ tapLocation: CGPoint) {
+        // Search the scene and not the world (https://stackoverflow.com/a/46189006/5071723)
+        let hitTestResults = sceneView.hitTest(tapLocation, options: nil)
+        guard let hitTestResult = hitTestResults.first else { return }
+        
+        let hitTestResultNode = hitTestResult.node
+        
+        let firstMaterialContents = (hitTestResultNode.geometry?.firstMaterial?.diffuse.contents as? UIColor)
+        if firstMaterialContents == Materials.detectedPlaneMaterial {
+            hitTestResultNode.geometry?.firstMaterial?.diffuse.contents = Materials.selectedPlaneMaterial
+        } else if firstMaterialContents == Materials.selectedPlaneMaterial {
+            hitTestResultNode.geometry?.firstMaterial?.diffuse.contents = Materials.dartboardMaterial
+            self.state = .playingDarts
+        }
+    }
+    
+    private func throwDart(_ tapLocation: CGPoint) {
+        
+        guard self.state == .playingDarts else { return }
+        
+        let hitTestResults = sceneView.hitTest(tapLocation, types: .existingPlaneUsingExtent)
+        guard let hitTestResult = hitTestResults.first else { return }
+        
+        // Start at the camera's point-of-view
+        let startPosition = self.sceneView.pointOfView!.position
+        // End on the plane
+        let endPosition = SCNMatrix4(hitTestResult.worldTransform).position
+        
+        // Setup the dart cone
+        let dartGeometry = SCNCone(topRadius: 0.0, bottomRadius: 0.01, height: 0.1)
+        // Make the cone red
+        dartGeometry.firstMaterial!.diffuse.contents = UIColor.red
+        // Add a white sheen
+        dartGeometry.firstMaterial!.specular.contents = UIColor.white
+        // Place the dart in the scene
+        let dartNode = SCNNode(geometry: dartGeometry)
+        let dartboardPlaneOrientation = SCNMatrix4(hitTestResult.worldTransform).orientation
+        dartNode.orientation = SCNQuaternion(
+            x: dartboardPlaneOrientation.x - self.sceneView.pointOfView!.orientation.x,
+            y: dartboardPlaneOrientation.y - self.sceneView.pointOfView!.orientation.y,
+            z: dartboardPlaneOrientation.z - self.sceneView.pointOfView!.orientation.z,
+            w: dartboardPlaneOrientation.w - self.sceneView.pointOfView!.orientation.w)
+        
+        // Animate the motion from the camera to the plane
+        CATransaction.begin()
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.fromValue = startPosition
+        animation.toValue = endPosition
+        animation.duration = 0.5  // seconds
+        CATransaction.setCompletionBlock {
+            dartNode.position = endPosition
+        }
+        CATransaction.commit()
+        
+        // Animate the dart's node
+        dartNode.addAnimation(animation, forKey: "throwDart")
+        
+        // Add the dart to the scene
+        self.sceneView.scene.rootNode.addChildNode(dartNode)
     }
 }
